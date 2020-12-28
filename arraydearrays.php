1f7f2264a17b84834ed96820cd0d0c679ccd44be
Igor Ferreira <ferreirabarbosaigor@gmail.com>
2020-12-28 12:28:17 -0300

    Atualizando arquivos

--- arraydearrays.php
+++ arraydearrays.php
@@ -1,5 +1,6 @@
 <?php
 
+// Array associativo que irei trabalhar com os exemplos
 
 $alunos = [
     $aluno1 = ["NOME" => "IGOR", "IDADE" => "24", "NOTA" => "7"],
@@ -19,14 +20,42 @@ foreach($alunos as $aluno) {
     }
 }
 
-// Exemplo básico de trabalho com array de arrays com array_walk_recursive
+// Exemplo básico de trabalho com array associativo com array_walk
+
 function testFunction($valor, $chave) {
     echo "A $chave tem o valor $valor\n";
     
 }
 
+array_walk($aluno, "testFunction");
 
-array_walk_recursive($alunos, "testFunction");
+// Exemplo básico de trabalho com array associativo com array_filter
+
+$alunosMQ7 = array_filter($alunos, function($aluno) {
+    return $aluno["NOTA"] >= 7;
+});
+
+print_r($alunosMQ7);
+
+// Exemplo básico de trabalho com array de arrays com array_key_exists, mas nesse exemplo irei usar o $aluno1
+// do array associativo $alunos
+
+if(array_key_exists("ALUNO", $aluno1)) {
+    echo "\nA chave NOME existe!";
+} else {
+    echo "\nA chave informada não existe!";
+}
+
+// Exemplo básico de trabalho com array de arrays com array_search, mas nesse exemplo irei usar o $aluno1 do
+// array associativo $alunos
+
+$chave = array_search("IGOR", $aluno1);
+
+if($chave != "NOME") {
+    echo "\n\nA chave não é NOME!";
+} else {
+    echo "\n\nA chave é NOME!";
+}
 
 ?>
 
